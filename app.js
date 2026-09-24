a7368970eb6b5a8e845d4190a06e076dfaf79b92
pedroluismartinezsan <pedroluismartinezsan@gmail.com>
2026-09-24 08:57:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-```javascript
+
 /* =========================================================
    UN DÍA MÁS
    APP.JS
@@ -612,4 +612,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 });
-```
+
